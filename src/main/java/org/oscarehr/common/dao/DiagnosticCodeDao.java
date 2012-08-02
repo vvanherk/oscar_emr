ccd50024ddbd79516196086330d671717b57cdcf
Jarrett Chisholm <j.chisholm@prylynx.com>
2012-08-02 17:15:34 -0400

    - [continued] work on AJAX look-ahead search for diagnostic codes diagnostic descriptions - [completed] work on Diagnostic Codes 'restful-like' GET method (getDiagnosticCodes.jsp) - [added] methods for getting DiagnosticCodes in getDiagnosticCodes.jsp
    Conflicts:
    	src/main/java/org/oscarehr/common/dao/DiagnosticCodeDao.java

--- src/main/java/org/oscarehr/common/dao/DiagnosticCodeDao.java
+++ src/main/java/org/oscarehr/common/dao/DiagnosticCodeDao.java
@@ -130,4 +130,23 @@ public class DiagnosticCodeDao extends AbstractDao<DiagnosticCode>{
 		List<DiagnosticCode> list = query.getResultList();
 		return list;
 	}
+	
+	public List<DiagnosticCode> findDiagnosticCodesByDescription(String description) {
+		Query query = entityManager.createQuery("select dc from DiagnosticCode dc where dc.description like (:description) order by dc.description");
+		query.setParameter("description", "%" + description + "%");
+
+		@SuppressWarnings("unchecked")
+		List<DiagnosticCode> list = query.getResultList();
+		return list;
+	}
+	
+	
+	public List<DiagnosticCode> findDiagnosticCodesByCode(String code) {
+		Query query = entityManager.createQuery("select dc from DiagnosticCode dc where dc.diagnosticCode like (:diagnostic_code) order by dc.diagnosticCode");
+		query.setParameter("diagnostic_code", code + "%");
+
+		@SuppressWarnings("unchecked")
+		List<DiagnosticCode> list = query.getResultList();
+		return list;
+	}
 }
