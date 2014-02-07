a79f31da862d28a9d9337c6c6f73caa4a6aa42e7
Jarrett Chisholm <j.chisholm@prylynx.com>
2014-02-07 12:06:27 -0500

    - missed some updates in previous commit (BM #1701)
    Change-Id: I6e3faedf2e05846c65fe482e4fea7054d34d7e81

--- database/mysql/updates/update-2014-02-07.2.sql
+++ database/mysql/updates/update-2014-02-07.2.sql
@@ -1,3 +1,8 @@
 ALTER TABLE allergies ADD INDEX (demographic_no);
 ALTER TABLE MyGroupAccessRestriction ADD INDEX (providerNo);
-
+ALTER TABLE casemgmt_note_link ADD INDEX (note_id);
+ALTER TABLE officeCommunication ADD INDEX (appointment_no);
+ALTER TABLE tickler_link ADD INDEX (tickler_no);
+ALTER TABLE professionalSpecialists ADD INDEX (referralNo);
+ALTER TABLE property ADD INDEX (provider_no);
+ALTER TABLE provider_default_program ADD INDEX (provider_no);
