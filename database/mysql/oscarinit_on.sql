9bf1266144e9c76c564a2dedfd95db477f7f01b9
Jarrett Chisholm <j.chisholm@prylynx.com>
2014-03-28 14:14:43 -0400

    - fixed issue with init script
    Change-Id: Icff02ed63c8e5fe61b20ef391f676ac01e0fc86c

--- database/mysql/oscarinit_on.sql
+++ database/mysql/oscarinit_on.sql
@@ -5857,21 +5857,6 @@ CREATE TABLE `formBCHP` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = '' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER formBCHP_formEdited BEFORE UPDATE on formBCHP FOR EACH ROW SET NEW.formEdited=NOW() */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `formCESD`
